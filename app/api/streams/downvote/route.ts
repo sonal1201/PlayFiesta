7afaafce9121845d92fdd214c5d7adab6f06cec9
Sonal Singh <sonal1201kumar@gmail.com>
2025-03-23 04:08:43 +0530

    Added upVote and DownVote Stream

---
+++ app/api/streams/downvote/route.ts
@@ -0,0 +1,49 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod"
+
+const DownVoteSchema = z.object({
+    streamId: z.string()
+})
+
+
+export async function POST(req: NextRequest){
+    const session = await getServerSession();
+
+    //can get rid of db call (optimization)
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email: session?.user?.email ?? ""
+        }
+    })
+
+    if(!user){
+        return NextResponse.json({
+            message: "Unauthenticated"
+        },{
+            status: 403
+        })
+    }
+
+    try {
+        const data =  DownVoteSchema.parse(await req.json());
+        await prismaClient.upvote.delete({
+            where:{
+                userId_streamId: {
+                    userId: user.id,
+                    streamId: data.streamId
+                }
+            }
+        })
+    }
+    catch (error) {
+        return NextResponse.json({
+            message:"Already Upvoated"
+        },{
+            status: 403
+        })
+    }
+
+}
+   
